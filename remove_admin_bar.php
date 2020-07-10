c0a1db6110690d5eb25204f50309cc0109361b8b
vsapountzis <evangelos@sapountzis.gr>
2020-07-10 22:30:22 +0300

    Update remove_admin_bar.php

--- remove_admin_bar.php
+++ remove_admin_bar.php
@@ -1,5 +1,9 @@
 <?php // Remove this PHP opening tag when pasting the following snippet on you child theme's functions.php file
 
-// Remove the admin bar from the front end
+// Hide admin bar for non admin users
 
-add_filter( 'show_admin_bar', '__return_false' );
+if (!current_user_can('manage_options')) {
+  
+	add_filter('show_admin_bar','__return_false');
+
+}
